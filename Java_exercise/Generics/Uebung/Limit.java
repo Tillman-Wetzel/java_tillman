58ad4885324e4f825e2b990ebfa15748f8d1be85
Tillman-Wetzel <tillman.wetzel@gmx.net>
2026-01-17 14:05:24 +0100

    letzer stand

---
+++ Java_exercise/Generics/Uebung/Limit.java
@@ -0,0 +1,24 @@
+package Java_exercise.Generics.Uebung;
+
+public class Limit<T extends Number>{
+
+    public static void main(String[] args) {
+        Integer[] intArray = {10, 20, 30};
+        Double[] doubleArray = {40.5, 50.5, 60.5};
+        Limit<Integer> intClass = new Limit<Integer>(intArray);
+        System.out.println(intClass.calcAverage());
+        Limit<Double> doubleClass = new Limit<Double>(doubleArray);
+        System.out.println(doubleClass.calcAverage());
+    }
+
+    private T[] arrayValue;
+
+    public Limit(T [] arrayValue) {
+        this.arrayValue = arrayValue;
+    }
+
+    double calcAverage() {
+        return ;
+    }
+    
+}
--- MySQL_LocalDatabase.session.sql
+++ MySQL_LocalDatabase.session.sql
@@ -1,50 +1,9 @@
-SELECT *
-FROM    customer;
+-- SELECT Customer.Name, Invoices.InvoiceDate, Invoices.Amount
+-- FROM customer
+-- LEFT OUTER JOIN invoices
+-- ON invoices.CustomerID = Customer.ID;
 
-SELECT *
-FROM    customer
-WHERE   Country = 'USA'
-AND     City LIKE 'Lo%';
-
-SELECT *
-FROM    customer
-WHERE   Country = 'USA';
-
-SELECT *
-FROM     customer
-ORDER BY Country ASC, ContactName DESC;
-
-SELECT *
-FROM    customer
-Where   (Country = 'USA' OR Country = 'UK' OR Country = 'Australia')
-And     (ContactName LIKE 'M%' OR ContactName LIKE 'R%' );
-
-Select *
-From    customer
-Where Not (ContactName LIKE 'M%' OR ContactName LIKE 'R%' );
-
-Select *
- From   customer
- Where  CompanyName IS NULL 
-    OR  ContactName IS NULL 
-    OR  Address     IS NULL 
-    OR  City        IS NULL 
-    OR  PostalCode  IS NULL 
-    OR  Country     IS NULL;
-
-Select *
- From   customer
- Where  CompanyName IS Not NULL 
-    AND ContactName IS Not NULL 
-    AND Address     IS Not NULL 
-    AND City        IS Not NULL 
-    AND PostalCode  IS Not NULL 
-    AND Country     IS Not NULL;
-
-SELECT  CompanyName AS NameOfCompany, ContactName AS UserName
-FROM    customer;
-
-SELECT COUNT(Country) AS NumberOfCountries, Country
-FROM    customer
-GROUP BY Country
-ORDER BY NumberOfCountries ASC;
+SELECT Customer.Name, Invoices.InvoiceDate, Invoices.Amount
+FROM invoices
+RIGHT OUTER JOIN customer
+ON invoices.CustomerID = Customer.ID;
